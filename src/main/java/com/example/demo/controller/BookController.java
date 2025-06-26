6f6cbe4fd69118bb2734717efbb98b7e235813ae
alice20240103 <alice20240103@gamil.com>
2025-06-26 16:10:22 +0900

    update post

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class BookController {
 		return dao.findAll();
 	}
 	
+	@PostMapping
+	public String save(Book b) {
+		dao.save(b);
+		return "OK";
+	}
 	
 }
